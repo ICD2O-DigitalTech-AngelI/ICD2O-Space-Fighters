de4fdde046e9398ceb97a477ecea47cfebf88c77
AngelI4 <168652607+AngelI4@users.noreply.github.com>
2024-05-07 23:24:30 +0000

    added changes to games.js

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 /*global Phaser*/
 
 const config = {
-  type: Phasor.AUTO,
-  width:1920
-  height:1080,
-  backgound:0x5f6e7a,
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgound: 0x9a9093
   }
 
 const game = new Phaser.Game(config)
-console.log.(game)
+console.log(game)
