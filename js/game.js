184829ed6ded721edd3df03dc85a6e66e2003734
AngelI4 <168652607+AngelI4@users.noreply.github.com>
2024-05-08 14:02:36 +0000

    added comment to our files

--- js/game.js
+++ js/game.js
@@ -1,11 +1,30 @@
 /*global Phaser*/
 
-const config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  backgound: 0x9a9093
+// Copyright (c) 2020 Mr. Coxall All rughts reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Phaser3 configurationfile
+
+//* Game scene */
+  const config = {      
+    type:Phaser.AUTO,
+    width:1920,
+    height:1080,
+    physics: {      
+     default: 'arcade',
+      arcade: {
+      debug: true
+      }
+    },
+    // set background color
+    backgroundcolor: 0x5f6e7a,
+     scale: { 
+       mode: Phaser.Scale.FIT,
+      // we place it in the middle of the page.
+      autoCenter: Phaser.Scale.CENTER_BOTH
   }
+}
 
 const game = new Phaser.Game(config)
 console.log(game)
